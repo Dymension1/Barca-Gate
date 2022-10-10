b39fff4a442f1e0fb358838ecd7d8393e4385fc7
Luis Ernesto Urias Chavarin <lu@gl7.mx>
2022-10-10 09:29:17 -0700

    pequeños cambios

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 function toogle() {
-  const header = document.querySelector("#header");
-  header.classList.toogle("active1");
+  // const header = document.querySelector("#header");
+  // header.classList.toogle("active1");
+
+  setTimeout(() => location.href = "https://www.fcbarcelona.es/es/", 1000);
+}
+
+
+$(document).ready(function () { 
+  $('.ir-arriba').click(function () { $('body,html').animate({ scrollTop: '0px' }, 1000); });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) { $('.ir-arriba').slideDown(600); } else { $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function () { $('body,html').animate({ scrollTop: '1000px' }, 1000); });
+
+
+}); //Hacia arriba
+
+function irArriba() {
+  $('.ir-arriba').click(function () { $('body,html').animate({ scrollTop: '0px' }, 1000); });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) { $('.ir-arriba').slideDown(600); } else { $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function () { $('body,html').animate({ scrollTop: '1000px' }, 1000); });
 }
