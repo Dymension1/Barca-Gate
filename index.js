5f964e3fadba89ff0090b9c00f9d2c1c664594d5
Luis Ernesto Urias Chavarin <lu@gl7.mx>
2022-11-26 01:06:35 -0700

    index fixed

--- index.js
+++ index.js
@@ -2,26 +2,24 @@ function toogle() {
   // const header = document.querySelector("#header");
   // header.classList.toogle("active1");
 
-  setTimeout(() => location.href = "https://www.fcbarcelona.es/es/", 1000);
+  setTimeout(() => (location.href = "https://www.fcbarcelona.es/es/"), 1000);
 }
 
-document.getElementById('chatbot').removeAttribute('left');
+let mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-$(document).ready(function () { 
-  $('.ir-arriba').click(function () { $('body,html').animate({ scrollTop: '0px' }, 1000); });
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 0) { $('.ir-arriba').slideDown(600); } else { $('.ir-arriba').slideUp(600); }
-  });
-  $('.ir-abajo').click(function () { $('body,html').animate({ scrollTop: '1000px' }, 1000); });
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-
-}); //Hacia arriba
-
-function irArriba() {
-  $('.ir-arriba').click(function () { $('body,html').animate({ scrollTop: '0px' }, 1000); });
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 0) { $('.ir-arriba').slideDown(600); } else { $('.ir-arriba').slideUp(600); }
-  });
-  $('.ir-abajo').click(function () { $('body,html').animate({ scrollTop: '1000px' }, 1000); });
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
